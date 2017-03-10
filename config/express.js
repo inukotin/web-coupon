e24ebea12d0b0b63c1278ce8687c9d08d268b673
Naren Inukoti <naren_inukoti@yahoo.com>
2017-03-10 03:44:54 -0500

    not working but starting building out basic angular front end

--- config/express.js
+++ config/express.js
@@ -10,23 +10,22 @@ var config = require('./config'),
 module.exports = function() {
     var app = express();
 
-    app.use(session({
-        saveUninitialized: true,
-        resave: true,
-        secret: 'cookie-secret'
-    }));
-
-    app.use(flash());
-
     app.use(bodyParser.urlencoded({
         extended: true
     }));
 
     app.use(bodyParser.json());
 
+    app.use(session({
+        saveUninitialized: true,
+        resave: true,
+        secret: 'cookie-secret'
+    }));
+
     app.set('views', './app/views');
     app.set('view engine', 'ejs');
 
+    app.use(flash());
     app.use(passport.initialize());
     app.use(passport.session());
 
--- public/app.js
+++ public/app.js
@@ -1,5 +1,12 @@
 var appName = "coupon-app";
-var app = angular.module(appName, []);
+var app = angular.module(appName, ['ngResource','ngRoute', 'example', 'users']);
+
+app.config(['$locationProvider', function($locationProvider) {
+        $locationProvider.hashPrefix('!');
+    }
+]);
+
+if (window.location.hash === '#_=_') window.location.hash = '#!';
 
 angular.element(document).ready(function() {
     angular.bootstrap(document, [appName]);
